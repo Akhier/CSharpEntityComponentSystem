6c07a174520c35143a552752e95cb98ef7712099
akhier <akhierthedragonhearted@yahoo.com>
2014-01-15 20:56:49 -0500

    Initial commit Works with two components[Health, Coordinate], a System[Health], and the Entity Manager working

---
+++ C_HealthSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityComponentSystemTest
+{
+    class HealthSystem : ECSystem {
+/// <summary>
+        /// Sets the selected entity's HP to a new value
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to affect</param>
+        /// <param name="newhp">The new value to set the hp to</param>
+        static public void setHP (UInt32 entity, int newhp) {
+            EntityManager.componentsOnEntities[entity][ComponentName.Health].HP = newhp;
+        }
+
+        /// <summary>
+        /// If the selected entity has the HealthComponent set the max HP
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to affect</param>
+        /// <param name="maxhp">The max hp you want the entity to have as an int</param>
+        static public void setMaxHP (UInt32 entity, int maxhp) {
+            EntityManager.componentsOnEntities[entity][ComponentName.Health].maxHP = maxhp;
+        }
+
+        /// <summary>
+        /// Gets the selected entity's hp
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to check</param>
+        /// <returns>The entity's HP</returns>
+        static public int getHP(UInt32 entity) {
+            return EntityManager.componentsOnEntities[entity][ComponentName.Health].HP;
+        }
+
+        /// <summary>
+        /// Gets the selected entity's max hp
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to check</param>
+        /// <returns>The entity's maxHP</returns>
+        static public int getMaxHP(UInt32 entity) {
+            return EntityManager.componentsOnEntities[entity][ComponentName.Health].maxHP;
+        }
+
+        /// <summary>
+        /// Lowers selected entity's HP by selected amount
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to affect</param>
+        /// <param name="amount">The amount you want to change the HP by</param>
+        static public void lowerHP (UInt32 entity, int amount) {
+            EntityManager.componentsOnEntities[entity][ComponentName.Health].HP -= amount;
+        }
+
+        /// <summary>
+        /// Raises selected entity's HP by selected amount up to the max hp
+        /// </summary>
+        /// <param name="entity">The UInt32 ID of entity to affect</param>
+        /// <param name="amount">The amount you want to change the HP by</param>
+        static public void raiseHP (UInt32 entity, int amount) {
+            EntityManager.componentsOnEntities[entity][ComponentName.Health].HP += amount;
+            if (getHP(entity) > getMaxHP(entity)) {
+                setHP(entity, getMaxHP(entity));
+            }
+        }
+
+        /// <summary>
+        /// Checks if any entities with the HealthComponent has an HP less then 1 and does as required
+        /// </summary>
+        static public void checkDeaths() {
+            foreach (UInt32 entity in EntityManager.getEntitiesByComponent(ComponentName.Health))
+            {
+                if (getHP(entity) < 1) {
+                    //entity is dead so do death stuff
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityComponentSystemTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt32 player = EntityManager.addNewEntity();
+            EntityManager.addComponentToEntity(ComponentName.Health, player);
+            HealthSystem.setHP(player, 12);
+            Console.Write(HealthSystem.getHP(player));
+            HealthSystem.checkDeaths();
+            Console.ReadKey();
+        }
+    }
+}
