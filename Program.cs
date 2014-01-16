19533e875d2f463e4d6117e90bfe15639345c606
akhier <akhierthedragonhearted@yahoo.com>
2014-01-15 21:28:10 -0500

    Got Libtcod working in a basic form -Opens a libtcod window

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using libtcod;
 
 namespace EntityComponentSystemTest
 {
@@ -13,9 +14,14 @@ namespace EntityComponentSystemTest
             UInt32 player = EntityManager.addNewEntity();
             EntityManager.addComponentToEntity(ComponentName.Health, player);
             HealthSystem.setHP(player, 12);
-            Console.Write(HealthSystem.getHP(player));
             HealthSystem.checkDeaths();
-            Console.ReadKey();
+            TCODConsole.initRoot(80, 50, "test");
+            while (!TCODConsole.isWindowClosed()) {
+                TCODConsole.root.clear();
+                TCODConsole.root.putChar(40, 25, '@');
+                TCODConsole.flush();
+                TCODConsole.waitForKeypress(true);
+            }
         }
     }
 }
