4a238414160da08af863e89e0d542a0978bb7e7b
akhier <akhierthedragonhearted@yahoo.com>
2014-01-19 08:55:17 -0500

    +LibtcodRenderSystem started

---
+++ C_LibtcodRenderSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libtcod;
+
+namespace CSharpEntityComponentSystem
+{
+    class LibtcodRenderSystem {
+        static public LibtcodRenderSystem(int width, int height, string title) {
+            TCODConsole.initRoot(width, height, title);
+        }
+    }
+}
