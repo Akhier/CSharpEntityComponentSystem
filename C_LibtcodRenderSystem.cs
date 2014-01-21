7261575818eed92ceb5c2f1ef46875432ca6bf21
akhier <akhierthedragonhearted@yahoo.com>
2014-01-21 06:05:33 -0500

    *Render system respects display level now

--- C_LibtcodRenderSystem.cs
+++ C_LibtcodRenderSystem.cs
@@ -26,8 +26,44 @@ namespace CSharpEntityComponentSystem
                 }
             }
             List<UInt32> entitywithboth = EntityManager.getEntitiesInBothComponents(ComponentName.Coord, ComponentName.Display); 
+            List<UInt32> entitywithboth2 = new List<UInt32>();
             foreach (UInt32 entity in entitywithboth) {
-                TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
+                if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Tile)) {
+                    TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
+                }
+                else {
+                    entitywithboth2.Add(entity);
+                }
+            }
+            
+            entitywithboth = new List<UInt32>();
+
+            foreach (UInt32 entity in entitywithboth2) {
+                if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Feature)) {
+                    TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
+                    entitywithboth.Remove(entity);
+                }
+                else {
+                    entitywithboth.Add(entity);
+                }
+            }
+
+            entitywithboth2 = new List<UInt32>();
+            
+            foreach (UInt32 entity in entitywithboth) {
+                if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Item)) {
+                    TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
+                    entitywithboth.Remove(entity);
+                }
+                else {
+                    entitywithboth2.Add(entity);
+                }
+            }
+
+            foreach (UInt32 entity in entitywithboth2) {
+                if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Creature)) {
+                    TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
+                }
             }
         }
     }
