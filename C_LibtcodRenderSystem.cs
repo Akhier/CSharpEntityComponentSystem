0e67d5708a8054ff6a200ce667719b1c3c9bf009
akhier <akhierthedragonhearted@yahoo.com>
2014-01-21 19:38:48 -0500

    *render works so removed old commented code

--- C_LibtcodRenderSystem.cs
+++ C_LibtcodRenderSystem.cs
@@ -26,33 +26,6 @@ namespace CSharpEntityComponentSystem
                 }
             }
             List<UInt32> entitywithboth = EntityManager.getEntitiesInBothComponents(ComponentName.Coord, ComponentName.Display);
-            //List<UInt32> entitywithboth2 = new List<UInt32>();
-            //List<UInt32> entitywithboth3 = new List<UInt32>();
-            //foreach (UInt32 entity in entitywithboth) {
-            //    if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Tile)) {
-            //        TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
-            //    }
-            //    else {
-            //        entitywithboth2.Add(entity);
-            //    }
-            //}
-            
-            //foreach (UInt32 entity in entitywithboth2) {
-            //    if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Item)) {
-            //        TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
-            //        entitywithboth.Remove(entity);
-            //    }
-            //    else {
-            //        entitywithboth3.Add(entity);
-            //    }
-            //}
-            
-            //foreach (UInt32 entity in entitywithboth3) {
-            //    if (EntityManager.componentsOnEntities[entity][ComponentName.Display].Render && (EntityManager.componentsOnEntities[entity][ComponentName.Display].displaylevel == DisplayLevel.Creature)) {
-            //        TCODConsole.root.putChar(EntityManager.componentsOnEntities[entity][ComponentName.Coord].X, EntityManager.componentsOnEntities[entity][ComponentName.Coord].Y, EntityManager.componentsOnEntities[entity][ComponentName.Display].DisplayIcon);
-            //        entitywithboth.Remove(entity);
-            //    }
-            //}
             Dictionary<CoordinateComponent, UInt32> renderedEntities = new Dictionary<CoordinateComponent, UInt32>();
             foreach (UInt32 entity in entitywithboth) {
                 DisplayComponent entitydisplay = EntityManager.componentsOnEntities[entity][ComponentName.Display];
